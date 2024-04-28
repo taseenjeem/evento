e180b6cd78183c87bb565ec07d41e2aa0f2be67e
taseenjeem <taseenjeem.dev@gmail.com>
2024-04-28 14:17:43 +0600

    added payment form

---
+++ app/payment/page.js
@@ -0,0 +1,16 @@
+import PaymentForm from "@/components/payment-page/PaymentForm";
+
+const PaymentPage = () => {
+  return (
+    <>
+      <section className="container">
+        <div className="bg-[#242526] p-6 rounded-lg max-w-xl mx-auto my-12">
+          <h2 className="font-bold text-xl mb-8">Payment Details</h2>
+          <PaymentForm />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default PaymentPage;
