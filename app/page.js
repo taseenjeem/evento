c4d96ff60b7d958858f7b24b006f113999225919
taseenjeem <taseenjeem.dev@gmail.com>
2024-04-28 13:58:58 +0600

    Added event list card

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import EventList from "@/components/root-page/EventList";
 import Header from "@/components/root-page/Header";
 
 const RootPage = () => {
   return (
     <>
       <Header />
+      <EventList />
     </>
   );
 };
