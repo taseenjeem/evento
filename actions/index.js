7031ad73c621011085d0931c20bc1b7370432c2b
taseenjeem <taseenjeem.dev@gmail.com>
2024-04-30 15:54:01 +0600

    added email

--- actions/index.js
+++ actions/index.js
@@ -1,13 +1,16 @@
 "use server";
 
+import EmailTemplate from "@/components/EmailTemplate";
 import {
   createUser,
   findUser,
+  getEventByID,
   updateEveInterest,
   updateGoing,
 } from "@/db/queries";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { Resend } from "resend";
 
 export const registerUser = async (formData) => {
   const user = Object.fromEntries(formData);
@@ -51,10 +54,31 @@ export const interestInEvent = async (eveID, authId) => {
 export const addGoingOnEvent = async (eveID, user) => {
   try {
     await updateGoing(eveID, user?.id);
+    await sendMail(eveID, user);
   } catch (error) {
     console.error(error);
     throw error;
   }
   revalidatePath("/");
   redirect("/");
+};
+
+export const sendMail = async (eveID, user) => {
+  try {
+    const event = await getEventByID(eveID);
+
+    const resend = new Resend(process.env.RS_API);
+
+    const message = `Dear ${user?.name}, you have been successfully registered for the event, ${event?.name}. Please carry this email and your official id to the venue. We are excited to have you here.`;
+
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: user?.email,
+      subject: "Successfully Registered for the event!",
+      react: EmailTemplate({ message }),
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
